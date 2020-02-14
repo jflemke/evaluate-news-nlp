ac27889a439d4059aeff0d300a1f7369a9d4a207
Johannes Flemke <Johannes.Flemke@bmw.de>
2020-02-14 09:39:23 +0100

    Setup aylien API

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,32 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+dotenv.config();
 
-const app = express()
+let path = require('path');
+const express = require('express');
+let aylien = require("aylien_textapi");
+const mockAPIResponse = require('./mockAPI.js');
 
-app.use(express.static('dist'))
+// set aylien API credentials
+const textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
 
-console.log(__dirname)
+const app = express();
+
+app.use(express.static('dist'));
+
+console.log(__dirname);
 
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-})
+    res.sendFile('dist/index.html');
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+    console.log('Example app listening on port 8080!');
+});
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+    res.send(mockAPIResponse);
+});
