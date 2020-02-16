9786937599394347f569493eb10ae251efa016a7
Johannes Flemke <Johannes.Flemke@bmw.de>
2020-02-16 19:52:16 +0100

    Setup communication between formSubmit and server

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,23 @@
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let formText = document.getElementById('url-input').value;
+    Client.checkForName(formText);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    console.log("::: Form Submitted :::");
+    fetch('http://localhost:8080/text/sentiment', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({url: formText})
+    })
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+        document.getElementById('results').innerHTML = `The polarity of the article ist ${res.polarity} and its subjectivity is ${res.subjectivity}.`;
+    });
 }
 
 export { handleSubmit }
